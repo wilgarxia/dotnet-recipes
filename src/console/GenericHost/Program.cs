d113a8c670869ae668cc52278c2f0ee060db2fcd
wilgarxia <wilgrxia@gmail.com>
2023-05-17 02:03:58 -0300

    Added GenericHost example

---
+++ src/console/GenericHost/Program.cs
@@ -0,0 +1,38 @@
+using GenericHost;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+try
+{
+    Log.Information("Starting host");
+
+    var builder = Host.CreateDefaultBuilder(args); 
+
+    builder.ConfigureServices(services =>
+        {
+            services.AddHostedService<Worker>();
+        })
+        .UseSerilog();
+
+    var host = builder.Build();
+
+    host.Run();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
