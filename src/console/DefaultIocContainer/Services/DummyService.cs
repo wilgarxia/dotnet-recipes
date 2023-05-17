be5f0a07c3f889b6a9f6776a589668968bb5025a
wilgarxia <wilgrxia@gmail.com>
2023-05-16 22:55:11 -0300

    Renamed file

---
+++ src/console/DefaultIocContainer/Services/DummyService.cs
@@ -0,0 +1,17 @@
+namespace DefaultIocContainer.Services;
+
+public interface IDummyService
+{
+    Task DoSomeWork(CancellationToken ct);
+}
+
+public class DummyService : IDummyService
+{
+    public async Task DoSomeWork(CancellationToken ct)
+    {
+        if (ct.IsCancellationRequested)
+            return;
+
+        await Task.Delay(500);
+    }
+}
